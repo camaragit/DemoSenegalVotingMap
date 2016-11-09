34292917c8ea15832187f8e664fd65db58412fca
camaragit <camaradame@gmail.com>
2016-11-09 09:03:50 +0000

    premier commit

---
+++ donnees.js
@@ -0,0 +1,29 @@
+var resultatParti={"votes":[
+{"region":"Kolda","resultat":{"PDS":10,"PS":9388,"APR":26079}}, 
+{"region":"Louga","resultat":{"PDS":76301,"PS":23623,"APR":55266}},
+{"region":"Dakar","resultat":{"PDS":189493,"AFP":131444,"APR":205338}},
+{"region":"Ziguinchor","resultat":{"PDS":42968,"AFP":16511,"APR":17832}},
+{"region":"Thies","resultat":{"PDS":97590,"PS":72394,"REWMI":85404}},
+{"region":"Tambacounda","resultat":{"PDS":45419,"PS":8716,"APR":28768}},
+{"region":"Saint-Louis","resultat":{"PDS":85272,"PS":24956,"APR":60229}},
+{"region":"Matam","resultat":{"PDS":41205,"AFP":5474,"APR":54954}},
+{"region":"Kaolack","resultat":{"PDS":48255,"AFP":66342,"APR":33768}},
+{"region":"Fatick","resultat":{"PDS":36862,"AFP":17860,"APR":55839}},
+{"region":"Diourbel","resultat":{"PDS":98357,"PS":11719,"APR":61725}},
+{"region":"Sedhiou","resultat":{"PDS":50234,"PS":5407,"APR":14531}},
+{"region":"Kaffrine","resultat":{"PDS":42660,"AFP":20980,"APR":16868}},
+{"region":"Kedougou","resultat":{"PDS":13882,"AFP":1613,"APR":4656}},
+{"region":"BOUNKILING","resultat":{"PDS":12,"AFP":15,"APR":27}},
+{"region":"GOUDOMP","resultat":{"PDS":12,"AFP":15,"APR":13}},
+{"region":"SEDHIOU","resultat":{"PDS":12,"AFP":15,"APR":18}},
+{"region":"FOUNDIOUGNE","resultat":{"PDS":19,"AFP":15,"APR":15}},
+{"region":"FATICK","resultat":{"PDS":26,"AFP":15,"APR":15}},
+{"region":"KAFFRINE","resultat":{"PDS":12,"AFP":15,"APR":15}},
+{"region":"MATAM","resultat":{"PDS":12,"AFP":15,"APR":15}}
+]};
+
+var couleurParti={
+"PDS":"blue",
+"PS":"green",
+"APR":"brown",
+"AFP":"yellow"};
